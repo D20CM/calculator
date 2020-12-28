0faa6087000b498d38a73d9ba7495a2f7eede1ed
D20CM <scottbrown500@hotmail.com>
2020-12-28 17:10:38 +0000

    make digit pads components

--- src/App.js
+++ src/App.js
@@ -1,49 +1,96 @@
 import React from 'react';
 import './App.css';
 
-class App extends React.Component{
+class App extends React.Component {
   constructor(props) {
-      super(props);
-      this.state = {
-        displayText: 1234567890
-      };
-      
+    super(props);
+    this.state = {
+      displayText: ""
+    };
+    this.handleClear = this.handleClear.bind(this);
+    this.numberInput = this.numberInput.bind(this);
+    
+  }
+
+  handleClear() {
+    this.setState({
+      displayText: 0
+    });
+  }
+
+  numberInput(enteredValue) {
+
+    if (this.state.displayText[0] == 0  && this.state.displayText[1] !== "."){
+      this.setState({
+        displayText: this.state.displayText.substring(1) + "" + enteredValue
+      })
     }
-  
-    render(){
-  return (
-    <div className="App">
-     <h1>Scott's Shitty Calculator</h1>
-     <h2>Git test</h2>
-     <div id="calculator-frame">
-      <div id="display">
-        {this.state.displayText}
+
+    else{
+    this.setState({
+      displayText:  this.state.displayText + "" + enteredValue
+    });
+  }
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Scott's Shitty Calculator</h1>
+        <h2>Git test</h2>
+        <div id="calculator-frame">
+          <div id="display">
+            {this.state.displayText}
+          </div>
+          <div id="clear" className="pad" onClick={this.handleClear}>
+            AC
       </div>
-      <div id="clear" className="pad">
-        AC
+          <div className="operator pad" id="divide">/</div>
+          <div className="operator pad" id="multiply">*</div>
+          <div className="operator pad" id="add">+</div>
+          <div className="operator pad" id="subtract">-</div>
+          <DigitPad number={9} id="nine" action={this.numberInput}/>
+          <DigitPad number={8} id="eight" action={this.numberInput}/>
+          <DigitPad number={7} id="seven" action={this.numberInput}/>
+          <DigitPad number={6} id="six" action={this.numberInput}/>
+          <DigitPad number={5} id="five" action={this.numberInput}/>
+          <DigitPad number={4} id="four" action={this.numberInput}/>
+          <DigitPad number={3} id="three" action={this.numberInput}/>
+          <DigitPad number={2} id="two" action={this.numberInput}/>
+          <DigitPad number={1} id="one" action={this.numberInput}/>
+          <DigitPad number={0} id="zero" action={this.numberInput}/>
+          <DigitPad number={"."} id="decimal" action={this.numberInput}/>
+          <div className="pad" id="equals">=</div>
+
+
+        </div>
       </div>
-      <div className="operator pad" id="divide">/</div>
-      <div className="operator pad" id="multiply">*</div>
-      <div className="operator pad" id="add">+</div>
-      <div className="operator pad" id="subtract">-</div>
-      <div className="digit pad" id="nine">9</div>
-      <div className="digit pad" id="eight">8</div>
-      <div className="digit pad" id="seven">7</div>
-      <div className="digit pad" id="six">6</div>
-      <div className="digit pad" id="five">5</div>
-      <div className="digit pad" id="four">4</div>
-      <div className="digit pad" id="three">3</div>
-      <div className="digit pad" id="two">2</div>
-      <div className="digit pad" id="one">1</div>
-      <div className="digit pad" id="zero">0</div>
-      <div className="digit pad" id="decimal">.</div>
-      <div className="pad" id="equals">=</div>
-
-
-     </div>
-    </div>
-  );
+    );
+  }
 }
-}
-
 export default App;
+
+
+
+class DigitPad extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+    
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(){
+  this.props.action(this.props.number)
+  }
+
+  render() {
+    return (
+      
+      <div className="digit pad" id={this.props.id} onClick={this.handleClick}>{this.props.number}</div>
+      
+    )
+  }
+}
